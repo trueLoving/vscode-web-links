41900215c6def2b43e888758643ea760cd2d5e56
star sky <1176789241@qq.com>
2020-01-14 19:04:49 +0800

    基本完成删除功能

--- src/commands/deleteLinks.ts
+++ src/commands/deleteLinks.ts
@@ -8,6 +8,24 @@ import { WebLink } from "../webLinks";
  */
 export function deleteLink(webLink: WebLink) {
 
-    vscode.window.showInformationMessage(webLink.toString());
+    // vscode.window.showInformationMessage(webLink.toString());
+
+    const webLinks = vscode.workspace.getConfiguration().webLinks;
+
+    let index = -1;
+
+    webLinks.forEach((element: { label: string; url: string; }, i) => {
+        if (element.label == webLink.label) {
+            index = i;
+        }
+    });
+
+    if (index == -1) {
+        vscode.window.showErrorMessage("删除失败");
+        return;
+    } else {
+        vscode.workspace.getConfiguration().webLinks.splice(index, 1);
+        vscode.window.showInformationMessage("删除成功，请刷新一下");
+    }
 
 }
